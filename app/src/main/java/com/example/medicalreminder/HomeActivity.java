e49790a679703aa13c6ad900d340e569b31d2ab4
Yixin Wang <ywang@sprypoint.com>
2020-03-02 20:09:52 -0500

    add nav drawer

--- app/src/main/java/com/example/medicalreminder/HomeActivity.java
+++ app/src/main/java/com/example/medicalreminder/HomeActivity.java
@@ -1,18 +1,48 @@
 package com.example.medicalreminder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private NavController navController;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        //FirebaseAuth.getInstance().signOut();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        navController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupWithNavController((NavigationView) findViewById(R.id.nav_view), navController);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, drawerLayout);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+        return true;
     }
 }
