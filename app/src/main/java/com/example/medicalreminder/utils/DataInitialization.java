17e11a7582801dc82f97b9fb040a7122c98993d6
bnddnb <11622931+BNDDNB@users.noreply.github.com>
2020-04-18 14:17:00 -0400

    updated data initialization

--- app/src/main/java/com/example/medicalreminder/utils/DataInitialization.java
+++ app/src/main/java/com/example/medicalreminder/utils/DataInitialization.java
@@ -1,11 +1,16 @@
 package com.example.medicalreminder.utils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
 import com.example.medicalreminder.model.Clinic;
+import com.example.medicalreminder.model.Medicine;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
@@ -34,6 +39,49 @@ public class DataInitialization {
             @Override
             public void onSuccess(Void aVoid) {
                 Log.i("Init Clinic",clinic.getName());
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e("Init Clinic Failed", e.getMessage());
+            }
+        });
+    }
+    private static void initMedicine(){
+        DatabaseReference medRef = FirebaseDatabase.getInstance().getReference("Medicine");
+        String id_1 = medRef.push().getKey();
+        String id_2 = medRef.push().getKey();
+
+        String medName = "Trandolapril";
+        String prescNum = "98761234";
+        String direct = "One pill a day with or without food.";
+        String docName = "Dr. John Doe";
+        String userID = "";
+        String clinicID = "";
+        int quantity = 1;
+        int refillsRemaining = 0;
+        long expiryDate = 0L;
+        String string_date = "12-December-2021";
+        SimpleDateFormat f = new SimpleDateFormat("dd-MMM-yyyy");
+        try {
+            Date d = f.parse(string_date);
+            expiryDate = d.getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        Medicine med1 = new Medicine(id_1, medName, prescNum, direct,docName,userID,clinicID,
+                quantity, refillsRemaining,expiryDate);
+
+        setMedicine(medRef,med1);
+    }
+
+    private static void setMedicine(DatabaseReference dbRef, final Medicine med){
+        dbRef.child(med.getClinicId())
+                .setValue(med).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.i("Init Clinic",med.getName());
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
