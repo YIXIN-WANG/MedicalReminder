2f99031e3a62c4de1ac1928cfd38e38b2cbeac8b
Yixin Wang <ywang@sprypoint.com>
2020-04-18 22:45:56 -0400

    add medicine

--- app/src/main/java/com/example/medicalreminder/MedicationFragment.java
+++ app/src/main/java/com/example/medicalreminder/MedicationFragment.java
@@ -2,13 +2,37 @@ package com.example.medicalreminder;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.medicalreminder.adapter.ClinicItemListAdapter;
+import com.example.medicalreminder.adapter.MedicineItemListAdapter;
+import com.example.medicalreminder.model.Clinic;
+import com.example.medicalreminder.model.Medicine;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class MedicationFragment extends Fragment {
+
+    private List<Medicine> medicineList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private MedicineItemListAdapter medicineItemListAdapter;
+    private DatabaseReference dbMedRef;
 
     public MedicationFragment() {
         // Required empty public constructor
@@ -18,6 +42,34 @@ public class MedicationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_medication, container, false);
+        View view = inflater.inflate(R.layout.fragment_medication, container, false);
+
+        ((AppCompatActivity) Objects.requireNonNull(getActivity())).getSupportActionBar().setTitle("Medication");
+        recyclerView = view.findViewById(R.id.medicine_recycleView);
+
+        recyclerView.setHasFixedSize(true);
+        medicineItemListAdapter = new MedicineItemListAdapter(getActivity(), medicineList);
+        recyclerView.setAdapter(medicineItemListAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        dbMedRef = FirebaseDatabase.getInstance().getReference("Medicine");
+        dbMedRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                medicineList.clear();
+                for(DataSnapshot clinicSnapshot : dataSnapshot.getChildren()){
+                    Medicine medicine = clinicSnapshot.getValue(Medicine.class);
+                    medicineList.add(medicine);
+                }
+                medicineItemListAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("Retrieve Med Error:",databaseError.getMessage());
+            }
+        });
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/example/medicalreminder/adapter/MedicineItemListAdapter.java
@@ -0,0 +1,101 @@
+package com.example.medicalreminder.adapter;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.medicalreminder.R;
+import com.example.medicalreminder.model.Clinic;
+import com.example.medicalreminder.model.Medicine;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class MedicineItemListAdapter extends RecyclerView.Adapter<MedicineItemListAdapter.MedListViewHolder> {
+
+    private List<Medicine> medicineList;
+    private LayoutInflater layoutInflater;
+    private DatabaseReference dbClinicRef;
+
+    public MedicineItemListAdapter(Context context, List<Medicine> medicineList) {
+        layoutInflater = LayoutInflater.from(context);
+        this.medicineList = medicineList;
+    }
+
+    @NonNull
+    @Override
+    public MedListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = layoutInflater.inflate(R.layout.medicine_item, parent, false);
+        dbClinicRef = FirebaseDatabase.getInstance().getReference("Clinics");
+        return new MedListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MedListViewHolder holder, int position) {
+        final int itemPosition = position;
+        final Medicine medicineItem = medicineList.get(position);
+
+        holder.medNameTextView.setText(medicineItem.getName());
+        holder.medExpiryDateTextView.setText(new SimpleDateFormat("yyyy-MM-dd").format(new Date(medicineItem.getExpiryDate())));
+        holder.medPrescriptionNumber.setText(medicineItem.getPrescriptionNumber());
+        holder.medDirectionTextView.setText(medicineItem.getDirection());
+        holder.medQuantityTextView.setText(String.valueOf(medicineItem.getQuantity())+" tablets");
+        holder.medRefillTextView.setText(medicineItem.getRefillsRemaining() + " of 12");
+
+        dbClinicRef.child(medicineItem.getClinicId()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Clinic clinic = dataSnapshot.getValue(Clinic.class);
+                holder.medClinicTextView.setText(clinic.getName());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("Med Clinics Error:",databaseError.getMessage());
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return medicineList.size();
+    }
+
+    public class MedListViewHolder extends RecyclerView.ViewHolder{
+        TextView medNameTextView;
+        TextView medExpiryDateTextView;
+        TextView medPrescriptionNumber;
+        TextView medDirectionTextView;
+        TextView medDoctorTextView;
+        TextView medRefillTextView;
+        TextView medQuantityTextView;
+        TextView medClinicTextView;
+
+        MedListViewHolder(View itemView) {
+            super(itemView);
+            medNameTextView = itemView.findViewById(R.id.medicineName);
+            medExpiryDateTextView = itemView.findViewById(R.id.medExpiryDate);
+            medPrescriptionNumber = itemView.findViewById(R.id.medPrescriptionNum);
+            medDirectionTextView = itemView.findViewById(R.id.medDirection);
+            medDoctorTextView = itemView.findViewById(R.id.medDoctor);
+            medRefillTextView = itemView.findViewById(R.id.medRefills);
+            medQuantityTextView = itemView.findViewById(R.id.medQuantity);
+            medClinicTextView = itemView.findViewById(R.id.medClinic);
+        }
+    }
+
+}
