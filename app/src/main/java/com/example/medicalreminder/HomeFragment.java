3810d0cfbde3ab6f713f64c6e7b98ab5f019e131
bnddnb <11622931+BNDDNB@users.noreply.github.com>
2020-04-19 15:15:33 -0400

    update home item

--- app/src/main/java/com/example/medicalreminder/HomeFragment.java
+++ app/src/main/java/com/example/medicalreminder/HomeFragment.java
@@ -1,28 +1,48 @@
 package com.example.medicalreminder;
 
-
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TimePicker;
 
+import com.example.medicalreminder.adapter.HomeItemListAdapter;
+import com.example.medicalreminder.model.Reminder;
 import com.github.techisfun.onelinecalendar.DateSelectionListener;
 import com.github.techisfun.onelinecalendar.OneLineCalendarView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Objects;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements HomeItemListAdapter.OnReminderItemListener {
 
+    private List<Reminder> remList = new ArrayList<>();
     private View fragView;
+    private RecyclerView recyclerView;
+    private HomeItemListAdapter homeItemListAdapter;
+    private DatabaseReference dbRemRef;
+    private TimePicker picker;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -33,15 +53,39 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         fragView = inflater.inflate(R.layout.fragment_home, container, false);
-        final OneLineCalendarView calendarView = (OneLineCalendarView) fragView.findViewById(R.id.calendar_view);
+        ((AppCompatActivity) Objects.requireNonNull(getActivity())).getSupportActionBar().setTitle("Home");
+        recyclerView = fragView.findViewById(R.id.home_recycleView);
 
+        recyclerView.setHasFixedSize(true);
+        homeItemListAdapter = new HomeItemListAdapter(getActivity(), remList, this);
+        recyclerView.setAdapter(homeItemListAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        dbRemRef = FirebaseDatabase.getInstance().getReference("Reminder");
+        dbRemRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                remList.clear();
+                for(DataSnapshot clinicSnapshot : dataSnapshot.getChildren()){
+                    Reminder rem = clinicSnapshot.getValue(Reminder.class);
+                    remList.add(rem);
+                }
+                homeItemListAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("Retrieve Clinics Error:",databaseError.getMessage());
+            }
+        });
+
+        final OneLineCalendarView calendarView = (OneLineCalendarView) fragView.findViewById(R.id.calendar_view);
         calendarView.setOnDateClickListener(new DateSelectionListener() {
             @Override
             public boolean onDateSelected(@NonNull Date date) {
                 calendarView.setStickyHeaderText(SimpleDateFormat.getDateInstance().format(date));
                 return true;
             }
-
 
             @Override
             public boolean onDateUnselected() {
@@ -51,5 +95,9 @@ public class HomeFragment extends Fragment {
         });
         return fragView;
     }
-
+    @Override
+    public void onOpenTextClick(Reminder rem) {
+//        picker=(TimePicker)frag.findViewById(R.id.timePicker1);
+//        picker.setIs24HourView(true);
+    }
 }
---
+++ app/src/main/java/com/example/medicalreminder/adapter/HomeItemListAdapter.java
@@ -0,0 +1,109 @@
+package com.example.medicalreminder.adapter;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.medicalreminder.HomeFragment;
+import com.example.medicalreminder.R;
+import com.example.medicalreminder.model.Medicine;
+import com.example.medicalreminder.model.Reminder;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+public class HomeItemListAdapter extends RecyclerView.Adapter<HomeItemListAdapter.HomeListViewHolder> {
+
+    private List<Reminder> remList;
+    private LayoutInflater layoutInflater;
+    private HomeFragment homeFragment;
+    private DatabaseReference medRef;
+    private DatabaseReference remRef;
+
+    public HomeItemListAdapter(Context context, List<Reminder> remList, HomeFragment homeFragment) {
+        layoutInflater = LayoutInflater.from(context);
+        this.remList = remList;
+        this.homeFragment = homeFragment;
+    }
+
+    @NonNull
+    @Override
+    public HomeListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = layoutInflater.inflate(R.layout.home_item, parent, false);
+        medRef = FirebaseDatabase.getInstance().getReference("Medicine");
+        remRef = FirebaseDatabase.getInstance().getReference("Reminder");
+        return new HomeListViewHolder(view, homeFragment);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HomeListViewHolder holder, int position) {
+        final int itemPosition = position;
+        final Reminder remItem = remList.get(position);
+
+        String histTime = "Taken at continue next at UNKNOWN";
+        Long timeHist = remItem.getScheduleTime();
+        DateFormat formatter = new SimpleDateFormat("HH:mm");
+        String timeFormated = formatter.format(timeHist);
+        holder.timeMedSchedule.setText(timeFormated);
+        holder.medTakenTime.setText(histTime);
+
+        Query q1 = medRef.child(remItem.getMedicineId());
+        q1.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Medicine med = dataSnapshot.getValue(Medicine.class);
+                holder.medNameTitle.setText(med.getName());
+                holder.instructionText.setText(med.getDirection());
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("Medicine query error:", databaseError.getMessage());
+            }
+        });
+
+        holder.openTimePickerTextView.setOnClickListener(view -> {
+            holder.onClinicItemListener.onOpenTextClick(remList.get(itemPosition));
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return remList.size();
+    }
+
+    public class HomeListViewHolder extends RecyclerView.ViewHolder{
+        TextView medNameTitle;
+        TextView timeMedSchedule;
+        TextView instructionText;
+        TextView medTakenTime;
+        TextView openTimePickerTextView;
+        private OnReminderItemListener onClinicItemListener;
+
+        HomeListViewHolder(View itemView, OnReminderItemListener onClinicItemListener) {
+            super(itemView);
+            this.onClinicItemListener = onClinicItemListener;
+            medNameTitle = itemView.findViewById(R.id.medNameTitle);
+            timeMedSchedule = itemView.findViewById(R.id.timeMedSchedule);
+            instructionText = itemView.findViewById(R.id.instructionText);
+            medTakenTime = itemView.findViewById(R.id.medTakenTime);
+            openTimePickerTextView = itemView.findViewById(R.id.timeMedSchedule);
+        }
+    }
+
+    public interface OnReminderItemListener {
+        void onOpenTextClick(Reminder rem);
+    }
+}
